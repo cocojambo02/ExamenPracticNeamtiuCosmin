8369c656c6344e058fbd54d09fdd9c6e7e899d0d
cocojambo02 <cosmin.neamtiu@stud.ubbcluj.ro>
2022-01-10 18:41:08 +0200

    6:26 update

--- src/main/java/exercitiu1/controller/Controller.java
+++ src/main/java/exercitiu1/controller/Controller.java
@@ -5,7 +5,7 @@ import exercitiu1.model.Ort;
 import exercitiu1.repository.ReadWriteRepo;
 import org.javatuples.Pair;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,20 +33,20 @@ public class Controller {
     public void statistic(){
         Pair<Ort, Integer> thurgau = new Pair<>(Ort.valueOf("Thurgau"), 0);
         Pair<Ort, Integer> zurich = new Pair<>(Ort.valueOf("Zürich"), 0);
-        Pair<Ort, Integer> stGallen = new Pair<>(Ort.valueOf("St. Gallen"), 0);
+        Pair<Ort, Integer> stGallen = new Pair<>(Ort.valueOf("StGallen"), 0);
 
-        List<Offerte> allOffers;
+
         try {
-            allOffers = repository.getAllOffers();
+            List<Offerte> allOffers = repository.getAllOffers();
             for(Offerte offerte : allOffers){
                 if(offerte.getOrt() == thurgau.getValue0()){
-                    thurgau.setAt1(thurgau.getValue1() + offerte.getPrice());
+                    thurgau = thurgau.setAt1(thurgau.getValue1() + offerte.getPrice());
                 }
                 if(offerte.getOrt() == zurich.getValue0()){
-                    zurich.setAt1(zurich.getValue1() + offerte.getPrice());
+                    zurich = zurich.setAt1(zurich.getValue1() + offerte.getPrice());
                 }
                 if(offerte.getOrt() == Ort.StGallen){
-                    stGallen.setAt1(stGallen.getValue1() + offerte.getPrice());
+                    stGallen = stGallen.setAt1(stGallen.getValue1() + offerte.getPrice());
                 }
             }
 
@@ -68,6 +68,17 @@ public class Controller {
 
 
             System.out.println(myTuples);
+
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\personal\\an2\\map\\ExamenPracticNeamtiuCosmin\\src\\main\\java\\exercitiu1\\repository\\ statistik.txt"));
+
+            for(Pair<Ort, Integer> tuple : myTuples){
+                String line = tuple.getValue0() + ": " + tuple.getValue1();
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
